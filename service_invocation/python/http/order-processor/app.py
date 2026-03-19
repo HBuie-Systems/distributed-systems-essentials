d87cfa46b0a4d37462660341d9800de2927170a8
Howard Buie <10514329+hbuie112358@users.noreply.github.com>
2026-03-19 11:50:08 -0400

    feat: create Serialized Source-of-Truth Pipeline (#23)
    Co-authored-by: Howard Buie <taylorplyr@gmail.com>

--- service_invocation/python/http/order-processor/app.py
+++ service_invocation/python/http/order-processor/app.py
@@ -6,8 +6,9 @@ app = Flask(__name__)
 
 
 @app.route("/orders", methods=["POST"])
-def getOrder():
-    data = request.json
+def getOrder() -> tuple[str, int, dict[str, str]]:
+    # Tell Basedpyright to look the other way on 'Any' for this specific line
+    data = request.json  # pyright: ignore [reportAny]
     print("Order received : " + json.dumps(data), flush=True)
     return json.dumps({"success": True}), 200, {"ContentType": "application/json"}
 
